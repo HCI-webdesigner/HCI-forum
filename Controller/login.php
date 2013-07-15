122d8359d3f66eca91abb765da6024b47536afdf
Sherlock Zhong <sherlock.z0917@gmail.com>
2013-07-15 16:48:39 +0800

    ajax login

--- Controller/login.php
+++ Controller/login.php
@@ -1,3 +1,13 @@
 <?php
-	
+
+	$usr = $_GET["usr"];
+	$pwd = $_GET["pwd"];//待修改
+	if($usr=="admin" && $pwd=="admin"){
+		$sendBack = array('correct' => 'yes','usr'=>$usr );
+	}
+	else{
+		$sendBack = array('correct' => 'no','usr'=>'' );
+	}
+	echo json_encode($sendBack);
+
 ?>
---
+++ javascripts/index.js
@@ -0,0 +1,44 @@
+window.onload = function(){
+	ajaxLogin();
+}
+
+function ajaxLogin(){
+	var button = document.getElementById("submit-button");
+	button.onclick = startLogin;//待修改
+}
+
+function startLogin(){
+	var usr = window.document.login_form.usr.value;
+	var pwd = window.document.login_form.pwd.value;
+	var url = "../HCI-forum/Controller/login.php?usr="+usr+"&pwd="+pwd;
+	postRequest(url);
+}
+
+function postRequest(urlStr){
+	var xmlHttp;
+	if(window.XMLHttpRequest){
+		xmlHttp = new XMLHttpRequest();
+	}
+	else if(window.ActiveXObject){
+		xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlHttp.open('POST',urlStr,true);
+	xmlHttp.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+	xmlHttp.onreadystatechange = function(){
+		if(xmlHttp.readyState == 4){
+			updatePage(xmlHttp.responseText);
+		}
+	}
+	xmlHttp.send(urlStr);
+}
+
+function updatePage(str){
+	var obj = eval ("(" + str + ")");
+	var login_box = document.getElementById("login");
+	if(obj.correct == "yes"){
+		login_box.innerHTML = "Hello," + obj.usr;
+	}
+	else{
+		alert("登录失败");
+	}
+}
