16cb5f2571f2b63bda40f9897e7da64917235e20
Sherlock Zhong <sherlock.z0917@gmail.com>
2013-07-15 14:49:40 +0800

    finished the 'area' table

--- SQL/area.sql
+++ SQL/area.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 15, 2013 at 02:09 PM
+-- Generation Time: Jul 15, 2013 at 02:47 PM
 -- Server version: 5.5.31-0ubuntu0.13.04.1
 -- PHP Version: 5.4.9-4ubuntu2.1
 
@@ -32,7 +32,20 @@ CREATE TABLE IF NOT EXISTS `area` (
   `count` int(11) NOT NULL,
   `icon_url` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `area`
+--
+
+INSERT INTO `area` (`id`, `name`, `count`, `icon_url`) VALUES
+(1, '前端开发', 0, '../HCI-forum/images/front.png'),
+(2, '后台开发', 0, '../HCI-forum/images/backstage.png'),
+(3, '移动开发', 0, '../HCI-forum/images/mobile.png'),
+(4, '系统测试', 0, '../HCI-forum/images/os.png'),
+(5, '资源共享', 0, '../HCI-forum/images/resource.png'),
+(6, '职场资讯', 0, '../HCI-forum/images/Jobs.png'),
+(7, '灌水专区', 0, '../HCI-forum/images/chat.png');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
