da437a04ef35b80c685fc292eb5cb8e7e142e3a0
Sherlock Zhong <sherlock.z0917@gmail.com>
2013-07-16 13:22:00 +0800

    added session to login

--- Controller/login.php
+++ Controller/login.php
@@ -4,13 +4,23 @@
 	$pwd = $_GET["pwd"];
 	$pwd = md5($pwd);
 	$conn = new DataAccess("localhost","root","zsl0917zsl","hciForum");
-	$sql = "SELECT password FROM `user` WHERE account='".$usr."' AND password='".$pwd."'";
-	$result = mysql_query($sql);
-	$match = mysql_num_rows($result);
+	$sql1 = "SELECT password FROM `user` WHERE account='".$usr."' AND password='".$pwd."'";
+	$result1 = mysql_query($sql1);
+	$match = mysql_num_rows($result1);
 	if($match){
-		$sendBack = array('correct' => 'yes','usr'=>$usr );
+		$sql2 = "SELECT * FROM `user` WHERE account='".$usr."'";
+		$result2 = mysql_query($sql2);
+		$info = mysql_fetch_array($result2);
+		$score = $info['score'];
+		$level = $info['level'];
+		$auth = $info['authority'];
 		session_start();
-		setcookie("usrName", $usr);
+		$_SESSION['usr'] = $usr;
+		$_SESSION['score'] = $score;
+		$_SESSION['level'] = $level;
+		$_SESSION['auth'] = $auth;
+		setcookie("usr", $usr);
+		$sendBack = array('correct' => 'yes','usr'=>$usr );
 	}
 	else{
 		$sendBack = array('correct' => 'no','usr'=>'' );
