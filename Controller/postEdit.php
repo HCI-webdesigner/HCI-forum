c85cecc6eb9a9fdb410e0553fcf347df6b7f77af
hhqcontinue <hhqcontinue@gmail.com>
2013-07-16 15:05:51 +0800

    sql

---
+++ Controller/postEdit.php
@@ -0,0 +1,13 @@
+<?php
+	include_once('/var/www/HCI-forum/Model/DataAccess.php');
+	$conn = new DataAccess("localhost", "root", "root", "hciForum");
+	class postEdit {
+		function __construct($title, $content, $type, $point) {
+			$current_userName = $_SESSION['usr'];
+			$current_boardName = $_GET['board'];
+			$current_user_id=mysql_query("SELECT id FROM user WHERE account='$current_userName'");
+			$current_board_id=mysql_query("SELECT id FROM board WHERE name='$current_boardName'");
+			
+		}
+	}
+?>
