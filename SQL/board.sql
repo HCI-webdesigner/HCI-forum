fc1b0f013d985b3235b961ecddaa3630028b52d0
Sherlock Zhong <sherlock.z0917@gmail.com>
2013-07-16 15:39:18 +0800

    new database

--- SQL/board.sql
+++ SQL/board.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 14, 2013 at 04:28 PM
+-- Generation Time: Jul 16, 2013 at 03:38 PM
 -- Server version: 5.5.31-0ubuntu0.13.04.1
 -- PHP Version: 5.4.9-4ubuntu2.1
 
@@ -33,7 +33,38 @@ CREATE TABLE IF NOT EXISTS `board` (
   `area_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `area_id` (`area_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=27 ;
+
+--
+-- Dumping data for table `board`
+--
+
+INSERT INTO `board` (`id`, `name`, `moderator`, `area_id`) VALUES
+(1, 'HTML/HTML5', '', 1),
+(2, 'CSS', '', 1),
+(3, 'Javascript', '', 1),
+(4, '设计与美工', '', 1),
+(5, 'Java', '', 2),
+(6, 'PHP', '', 2),
+(8, 'JSP', '', 2),
+(9, 'Python', '', 2),
+(10, '框架', '', 2),
+(11, 'Android', '', 3),
+(12, 'IOS', '', 3),
+(13, '系统运维', '', 4),
+(14, '系统安全', '', 4),
+(15, '资料', '', 5),
+(16, '书籍', '', 5),
+(17, '软件', '', 5),
+(18, '其他', '', 5),
+(19, '实习资讯', '', 6),
+(20, '求职信息', '', 6),
+(21, '经验交流', '', 6),
+(22, '讲座活动', '', 6),
+(23, '其他', '', 6),
+(24, '情感', '', 7),
+(25, '娱乐', '', 7),
+(26, '其他', '', 7);
 
 --
 -- Constraints for dumped tables
