46511b562b4263ac87eb4b0b804f7922fdaf2d20
hhqcontinue <hhqcontinue@gmail.com>
2013-07-17 13:13:13 +0800

    post

--- Controller/allPost.php
+++ Controller/allPost.php
@@ -7,18 +7,29 @@
 	
 	class AllPost {
 		public $allPost = array();
-		function __construct($areaId) {
+		function __construct($areaId, $boardId) {
 			include_once('/var/www/HCI-forum/Model/DataAccess.php');
 			$conn = new DataAccess("localhost", "root", "root", "hciForum");
-			$sql = "SELECT post.* FROM post,board WHERE
-				post.board_id=board.id AND board.area_id='$areaId'";
-			$query = mysql_query($sql);
-			//echo $sql."<br>";
-			while($allPost=mysql_fetch_array($query)) {
-				$temp_user = mysql_fetch_array(mysql_query("SELECT account from user WHERE id='$allPost[user_id]'"));
-				echo $allPost['type'];
-				echo "<a href="."/HCI-forum/View/postView.php?id=$allPost[id]>".$allPost['title']."</a>";
-				echo "author :".$temp_user[0]."--"."post date: ".$allPost['post_date']."<br>";
+			if($boardId == 0) {
+				$sql = "SELECT post.* FROM post,board WHERE
+					post.board_id=board.id AND board.area_id='$areaId'";
+				$query = mysql_query($sql);
+				while($allPost=mysql_fetch_array($query)) {
+					$temp_user = mysql_fetch_array(mysql_query("SELECT account from user WHERE id='$allPost[user_id]'"));
+					echo $allPost['type'];
+					echo "<a href="."/HCI-forum/View/postView.php?id=$allPost[id]>".$allPost['title']."</a>";
+					echo "author :".$temp_user[0]."--"."post date: ".$allPost['post_date']."<br>";
+				}
+			}
+			else {
+				$sql = "SELECT post.* FROM post WHERE post.board_id='$boardId'";
+				$query = mysql_query($sql);
+				while($post_row=mysql_fetch_array($query)) {
+					$temp_user=mysql_fetch_array(mysql_query("SELECT account FROM `user` WHERE id='$post_row[user_id]'"));
+					echo $post_row['type'];
+					echo "<a href="."/HCI-forum/View/postView.php?id=$post_row[id]>".$post_row['title']."</a>";
+					echo "author :".$temp_user[0]."--"."post date: ".$post_row['post_date']."<br>";
+				}
 			}
 		}
 	}
