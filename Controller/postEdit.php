6fd19dd83c398c59d24999393a6c198378fbec0b
hhqcontinue <hhqcontinue@gmail.com>
2013-07-18 18:45:53 +0800

    edit,verify

--- Controller/postEdit.php
+++ Controller/postEdit.php
@@ -5,10 +5,13 @@
 		function __construct($title, $content) {
 			$current_userName = $_SESSION['usr'];
 			$current_boardName = $_GET['board'];
+			$current_auth = $_SESSION['auth'];
 			$current_user_id=mysql_query("SELECT id FROM user WHERE account='$current_userName'");
 			$current_board_id=mysql_query("SELECT id FROM board WHERE name='$current_boardName'");
 			$sql = "UPDATE `post` SET title='$title',content='$content'";
-			mysql_query($sql);
+			if(mysql_query($sql)) {
+				
+			}
 		}
 	}
 ?>
