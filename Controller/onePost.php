16eec04d2df1e4e3eb3c65712675684762eab471
hhqcontinue <hhqcontinue@gmail.com>
2013-07-19 21:34:24 +0800

    comment

--- Controller/onePost.php
+++ Controller/onePost.php
@@ -8,10 +8,20 @@
 	$sql = "SELECT * FROM `post` WHERE id='$postId'";
 	$query = mysql_query($sql);
 	while($post_rows = mysql_fetch_array($query)) {
+		$user = mysql_fetch_array(mysql_query("SELECT account FROM `user` WHERE id='$post_rows[user_id]'"));
+		$post_user = $user[0];
 		$post_title = $post_rows['title'];
 		$post_content = $post_rows['content'];
 	}
+	$comment_sql = "SELECT * FROM `comment` WHERE post_id='$postId'";
+	$comment_query = mysql_query($comment_sql);
+	$commentContent = array();
+	$commentUser = array();
+	while($comment_rows = mysql_fetch_array($comment_query)) {
+		array_push($commentContent, $comment_rows['content']);
+		$comment_user = mysql_fetch_array(mysql_query("SELECT account FROM `user` WHERE id='$comment_rows[user_id]'"));
+		array_push($commentUser, $comment_user[0]);
+	}
 	include_once('/var/www/HCI-forum/View/onePostView.php');
-
 	require_once("/var/www/HCI-forum/View/postFooter.html");
 ?>
--- View/onePostView.php
+++ View/onePostView.php
@@ -1,4 +1,14 @@
 			<div id="postTitle"><?php echo $post_title;?></div>
 			<div id="msg">
 				<?php echo $post_content;?>
+				<?php echo "<br>用户 : ".$post_user;?>
 			</div>
+			评论: 
+<?php
+	foreach ($commentUser as $key => $commentuser) {
+?>
+	<div id="comment_user"><?php echo $commentuser;?></div>
+	<div id="comment_content"><?php echo $commentContent[$key];?></div>
+<?php
+	}
+?>
