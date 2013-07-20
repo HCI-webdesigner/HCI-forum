2db3ff7ee754351b6534c4db693b7b2d08948393
Sherlock Zhong <sherlock.z0917@gmail.com>
2013-07-20 12:12:48 +0800

    fixed board view

--- View/allPostView.php
+++ View/allPostView.php
@@ -6,29 +6,28 @@
 				<li>
 					<a href="/HCI-forum/Controller/onePost.php?postId=<?php echo $postId[$key];?>" class="list-a">
 					<div class="post-link">
-					<span calss="author"><?php echo $user;?>:</span>
+					<span class="type">
+					[<?php
+					switch ($postType[$key]) {
+						case '0':
+							echo "普通帖";
+							break;
+						case '1':
+							echo "问答帖";
+							break;
+						case '2':
+							echo "辩论帖";
+							break;
+						case '3':
+							echo "精华帖";
+							break;
+						default:
+							break;
+					}?>]
+					</span>
+					<span calss="author">&nbsp;<?php echo $user;?>:</span>
 					<span class="title"><?php echo $postTitle[$key];?></span>
 					<span class="time"><?php echo $postDate[$key];?></span>
-					<span class="type">
-						<?php
-						switch ($postType[$key]) {
-							case '0':
-								echo "普通帖";
-								break;
-							case '1':
-								echo "问答帖";
-								break;
-							case '2':
-								echo "辩论帖";
-								break;
-							case '3':
-								echo "精华帖";
-								break;
-							default:
-								break;
-						}?>
-
-					</span>
 					</div>
 					</a>
 				</li>
