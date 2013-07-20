dc6b38b41c961018b36518b6744eed66736faf1c
Sherlock Zhong <sherlock.z0917@gmail.com>
2013-07-20 16:30:20 +0800

    update

--- Controller/addAreaController.php
+++ Controller/addAreaController.php
@@ -1,5 +1,7 @@
 <?php
-	include_once('/var/www/HCI-forum/Model/DataAccess.php');
+	define("DS",DIRECTORY_SEPARATOR);
+	define("ROOT",dirname(dirname(__FILE__)));
+	include_once(ROOT . DS . "Model" . DS . "DataAccess.php");
 
 	$conn = new DataAccess("hciForum");
 	if($_POST['sub']) {
--- Model/DataAccess.php
+++ Model/DataAccess.php
@@ -2,7 +2,7 @@
 	class DataAccess {
 		public $result;
 		function __construct($db) {
-			$this->result=mysql_connect("localhost", "root", "root");
+			$this->result=mysql_connect("localhost", "root", "zsl0917zsl");
 			mysql_select_db($db, $this->result);
 			mysql_query("set names 'utf8'");
 		}
