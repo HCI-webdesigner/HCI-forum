336d018da6cfa1937d13e7d25b33bcda2a0ff294
Sherlock Zhong <sherlock.z0917@gmail.com>
2013-07-20 20:53:34 +0800

    finally the comment function...

---
+++ Controller/saveComment.php
@@ -0,0 +1,19 @@
+<?php
+	
+	define("DS",DIRECTORY_SEPARATOR);
+	define("ROOT",dirname(dirname(__FILE__)));
+	include_once(ROOT . DS . "Model" . DS . "DataAccess.php");
+	$conn = new DataAccess("hciForum");
+
+	$content = $_POST['content'];
+	$date = date("Y-m-d");
+	$deleted = 0;
+	$postId = $_POST['postId'];
+	$usrId = $_POST['usrId'];
+
+	$sql = "INSERT INTO `comment`(`content`, `comment_date`, `deleted`,  `post_id`, `user_id`) 
+	VALUES ('$content','$date','$deleted','$postId','$usrId')";
+	mysql_query($sql);
+
+	echo '<script language=javascript>location="/HCI-forum/Controller/onePostCon.php?postId='.$postId.'"</script>';
+?>
---
+++ javascripts/comment.js
@@ -0,0 +1,49 @@
+window.onload = function(){
+	getInfo();
+};
+
+function getInfo(){
+	var info = XHR('/HCI-forum/Controller/checkLogin.php');
+	var form = document.getElementById('commentForm');
+	info.onreadystatechange = function(){
+		if(info.readyState == 4){
+			if(info.responseText){
+				var obj = eval("("+info.responseText+")");
+				var hiddenUsr = document.createElement("input");
+				hiddenUsr.type = 'text';
+				hiddenUsr.name = 'usrId';
+				hiddenUsr.value = obj.usrId;
+				hiddenUsr.style.display = "none";
+				form.appendChild(hiddenUsr);
+			}
+			else{
+				alert("您还没有登陆哦！！！");
+				location='/HCI-forum/';
+			}
+		}
+	}
+	var str=window.location.href;
+	var es=/postId=/; 
+	es.exec(str); 
+	var right=RegExp.rightContext; 
+	var hiddenPostId = document.createElement("input");
+	hiddenPostId.type = 'text';
+	hiddenPostId.name = 'postId';
+	hiddenPostId.value = right;
+	hiddenPostId.style.display = "none";
+	form.appendChild(hiddenPostId);
+}
+
+function XHR(urlStr){//XHR == XMLHttpRequest
+	var request;
+	if(window.XMLHttpRequest){
+		request = new XMLHttpRequest();
+	}
+	else if(window.ActiveXObject){
+		request = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	request.open('POST',urlStr,true);
+	request.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+	request.send(urlStr);
+	return request;
+}
