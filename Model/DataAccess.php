294263a93afd1ce38319445ed8bec4f5152bff81
hhqcontinue <hhqcontinue@gmail.com>
2013-07-21 23:23:34 +0800

    new log

--- Model/DataAccess.php
+++ Model/DataAccess.php
@@ -2,7 +2,7 @@
 	class DataAccess {
 		public $result;
 		function __construct($db) {
-			$this->result=mysql_connect("localhost", "root", "zsl0917zsl");
+			$this->result=mysql_connect("localhost", "root", "root");
 			mysql_select_db($db, $this->result);
 			mysql_query("set names 'utf8'");
 		}
