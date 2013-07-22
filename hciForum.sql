59491a4e3153c097acb4cb606c50da2bd70ddfe1
Sherlock Zhong <sherlock.z0917@gmail.com>
2013-07-22 15:09:44 +0800

    update database

--- hciForum.sql
+++ hciForum.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 22, 2013 at 02:04 PM
+-- Generation Time: Jul 22, 2013 at 03:09 PM
 -- Server version: 5.5.31-0ubuntu0.13.04.1
 -- PHP Version: 5.4.9-4ubuntu2.2
 
@@ -57,13 +57,13 @@ CREATE TABLE IF NOT EXISTS `area` (
 --
 
 INSERT INTO `area` (`id`, `name`, `count`, `icon_url`, `deleted`, `moderator`) VALUES
-(1, '前端开发2', 0, '/HCI-forum/images/front.png', 0, ''),
-(2, '后台开发', 0, '/HCI-forum/images/backstage.png', 0, ''),
-(3, '移动开发', 0, '/HCI-forum/images/mobile.png', 0, ''),
-(4, '系统测试', 0, '/HCI-forum/images/os.png', 0, ''),
-(5, '资源共享', 0, '/HCI-forum/images/resource.png', 0, ''),
-(6, '职场资讯', 0, '/HCI-forum/images/Jobs.png', 0, ''),
-(7, '灌水专区', 0, '/HCI-forum/images/chat.png', 0, ''),
+(1, '前端开发', 6, '/HCI-forum/images/front.png', 0, ''),
+(2, '后台开发', 2, '/HCI-forum/images/backstage.png', 0, ''),
+(3, '移动开发', 2, '/HCI-forum/images/mobile.png', 0, ''),
+(4, '系统测试', 3, '/HCI-forum/images/os.png', 0, ''),
+(5, '资源共享', 1, '/HCI-forum/images/resource.png', 0, ''),
+(6, '职场资讯', 2, '/HCI-forum/images/Jobs.png', 0, ''),
+(7, '灌水专区', 2, '/HCI-forum/images/chat.png', 0, ''),
 (15, 'HCI2', 0, '/HCI-forum/images/aboutus.jpg', 1, '');
 
 -- --------------------------------------------------------
@@ -78,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `board` (
   `area_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `area_id` (`area_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=27 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=26 ;
 
 --
 -- Dumping data for table `board`
@@ -127,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `comment` (
   PRIMARY KEY (`id`),
   KEY `post_id` (`post_id`),
   KEY `user1_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
 
 --
 -- Dumping data for table `comment`
@@ -151,7 +151,8 @@ INSERT INTO `comment` (`id`, `content`, `comment_date`, `deleted`, `post_id`, `u
 (17, '楼主好人一生平安！!!', '2013-07-17 00:00:00', 0, 7, 10),
 (18, '楼主好人一生平安！!!!', '2013-07-17 00:00:00', 0, 7, 13),
 (19, '楼主好人一生平安！!!!!', '2013-07-17 00:00:00', 0, 7, 10),
-(20, '沙发', '2013-07-22 14:02:28', 0, 10, 14);
+(20, '沙发', '2013-07-22 14:02:28', 0, 10, 14),
+(21, '呵呵', '2013-07-22 14:08:45', 0, 7, 14);
 
 -- --------------------------------------------------------
 
@@ -210,7 +211,7 @@ CREATE TABLE IF NOT EXISTS `post` (
   PRIMARY KEY (`id`),
   KEY `board_id` (`board_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
 
 --
 -- Dumping data for table `post`
@@ -223,7 +224,18 @@ INSERT INTO `post` (`id`, `title`, `content`, `type`, `point`, `deleted`, `post_
 (7, 'HTTP代理与 SPDY协议1', 'HTTP代理是最经典最常见的代理协议。其用途非常广泛，普遍见于公司内网环境，一般员工都需要给浏览器配置一个HTTP代理才能访问互联网。起初，HTTP代理也用来翻越G*W，但是因为G*W不断发展，普通的HTTP代理早已无效了。但是，基于仍然有不少人使用明文的HTTP代理协议结合 stunnel之类的软件进行加密翻Q，有时这种代理又被称为HTTPS代理。再后来，又出现了WebVpn via Spdy之类的代理协议，特点是Chrome浏览器直接支持。再加上HTTP代理协议可以代理什么？是只能代理HTTP还是也可以代理HTTPS，还是可 以用来实现SOCKS代理？总之，非常混乱。在fqsocks项目里用python实现了HTTP代理的各种主流变种，终于明白了不同称谓之后的真正含义。本文试图总结一二。\r\n\r\n代理基础\r\n\r\n所有的代理，其原理都是类似的。其网络拓扑结构都是这样的：\r\n\r\n[客户端] <-TCP连接-> [代理] <-TCP连接-> [服务器]\r\n代理左手拿着与客户端的连接，右手拿着与服务器的连接，然后在两个TCP连接之间做数据的对拷。各种不同的代理协议，不同的只是TCP连接之上跑的 是什么的协议，数据是怎么经过包装，拆包的。不存在客户端与服务器之间建立TCP连接的情况。只有VPN这种在IP包这一层工作的，才能实现客户端与服务 器的之间连接。', 0, 0, 0, '2013-07-16 00:00:00', 0, 0, 1, 10),
 (8, 'HTML5', 'HTML5是HTML下一個主要的修訂版本，現在仍處於發展階段。目標是取代1999年所製定的HTML 4.01和XHTML 1.0 標準，以期能在網際網路應用迅速發展的時候，使網路標準達到符合當代的網路需求。廣義論及HTML5時，實際指的是包括HTML、CSS和JavaScript在內的一套技術組合。它希望能夠減少瀏覽器對於需要外掛程式的豐富性網路應用服務（plug-in-based rich internet application，RIA)，如Adobe Flash、Microsoft Silverlight，與Oracle JavaFX的需求，並且提供更多能有效增強網路應用的標準集。\r\n具體來說，HTML5添加了許多新的語法特徵，其中包括<video>, <audio>, 和<canvas>元素，同時整合了SVG內容。這些元素是為了更容易的在網頁中添加和處理多媒體和圖片內容而添加的。其它新的元素包括<section>, <article>, <header>, 和<nav>,是為了豐富文件的資料內容。新的屬性的添加也是為了同樣的目的。同時也有一些屬性和元素被移除掉了。一些元素，像<a>, <cite>和<menu>被修改，重新定義或標準化了。同時APIs和DOM已經成為HTML5中的基礎部分了。[1]HTML5還定義了處理非法文件的具體細節，使得所有瀏覽器和客戶端程式能夠一致地處理語法錯誤。', 0, 0, 0, '2013-07-22 00:00:00', 0, 0, 1, 10),
 (9, '后台', '拍黄片', 1, 0, 0, '2013-07-22 00:00:00', 0, 0, 6, 14),
-(10, '移动', 'android', 1, 0, 0, '2013-07-22 14:02:15', 0, 0, 10, 14);
+(10, '移动', 'android', 1, 0, 0, '2013-07-22 14:02:15', 0, 0, 10, 14),
+(11, '系统', '运维', 3, 0, 0, '2013-07-22 14:19:24', 0, 0, 12, 14),
+(12, '灌水', '情感', 2, 0, 0, '2013-07-22 14:35:36', 0, 0, 23, 14),
+(13, '职场', '呵呵', 0, 0, 0, '2013-07-22 14:37:41', 0, 0, 19, 14),
+(14, '资源', '叔叔叔', 0, 0, 0, '2013-07-22 14:43:58', 0, 0, 15, 14),
+(15, '前端', '美工', 0, 0, 0, '2013-07-22 14:44:19', 0, 0, 4, 14),
+(16, '后台', '哈哈哈', 0, 0, 0, '2013-07-22 14:46:01', 0, 0, 8, 14),
+(17, '系统', '安全', 0, 0, 0, '2013-07-22 15:02:23', 0, 0, 13, 14),
+(18, '系统', '还是安全', 0, 0, 0, '2013-07-22 15:03:00', 0, 0, 13, 14),
+(19, '移动！！！', 'IOS！！！！', 0, 0, 0, '2013-07-22 15:03:27', 0, 0, 11, 14),
+(20, '职场。。。', '活动。。。', 0, 0, 0, '2013-07-22 15:06:32', 0, 0, 21, 14),
+(21, '灌水', '水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水水', 0, 0, 0, '2013-07-22 15:07:10', 0, 0, 25, 14);
 
 -- --------------------------------------------------------
 
@@ -248,7 +260,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`id`, `account`, `score`, `level`, `authority`, `password`) VALUES
 (10, 'hhq', 0, 0, 1, '9e8ca68bf7de888a350681d2f060c18e'),
 (13, '胡华泉', 0, 0, 0, '9e8ca68bf7de888a350681d2f060c18e'),
-(14, 'zsl', 0, 0, 1, '202cb962ac59075b964b07152d234b70');
+(14, 'zsl', 999, 999, 1, '202cb962ac59075b964b07152d234b70');
 
 -- --------------------------------------------------------
 
